cbf4990d241cfe8fb9e73da6b3a68a0cd338b7f4
Maitre_Guigz <guillaume.hirschler@gmail.com>
2025-01-03 13:47:41 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,60 +1,60 @@
-var go = function(){
-
-    var cam = document.getElementById("cam").value;
+const go = () => {
     const datetimeLocal = document.getElementById('datetime-local').value;
     const date = new Date(datetimeLocal);
 
     const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Les month commencent à 0
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Les mois commencent à 0
     const day = String(date.getDate()).padStart(2, '0');
     const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(Math.floor(now.getMinutes() / 5) * 5).padStart(2, '0');
+    const minutes = String(Math.floor(date.getMinutes() / 5) * 5).padStart(2, '0');
     
-    var laDate=year+"_"+month+"_"+day+"_"+hours+"_"+minutes+"_";
+    const laDate = `${year}_${month}_${day}_${hours}_${minutes}_`;
+    console.log(laDate);    
 
-    url="https://s3-eu-west-1.amazonaws.com/timelapsestorage/"+cam;
-    var div=document.getElementById("imgContainer");
+    const url = "https://s3-eu-west-1.amazonaws.com/timelapsestorage/";
+    const divs = document.querySelectorAll(".imgContainer");
 
-    div.innerHTML="<p>"+laDate+"</p>"
-    for(i=0; i<10; i++){
-        var a=document.createElement("a");
-        if(i<10){
-            var lienH = url+"/HIGH/"+laDate+"0"+i+".jpg";
-            var lienL = url+"/LOW/"+laDate+"0"+i+".jpg";
+    divs.forEach(div => {
+        const cam = div.id;
+        for (let i = 5; i < 10; i++) {
+            const a = document.createElement("a");
+            const suffix = i < 10 ? `0${i}` : i;
+            const lienH = `${url}${cam}/HIGH/${laDate}${suffix}.jpg`;
+            const lienL = `${url}${cam}/LOW/${laDate}${suffix}.jpg`;
+
+            a.href = lienH;
+            a.target = "_blank";
+            a.innerHTML = `
+                <figure class='zoom' onmousemove='zoom(event)' style='background-image: url(${lienH})'>
+                    <img src='${lienL}' onerror='this.parentElement.style.display="none"'/>
+                </figure>
+                <img src='${lienL}' style='height: 1px!important' onerror='this.style.display="none"'/>
+            `;
+            div.append(a);
         }
-        else{
-            var lienH = url+"/HIGH/"+laDate+i+".jpg";
-            var lienL = url+"/LOW/"+laDate+i+".jpg";
-        }
-        a.href=lienH;
-        a.target="_blank";
-        a.innerHTML="<figure class='zoom' onmousemove='zoom(event)' style='background-image: url("+lienH+")'><img src='"+lienL+"'/></figure>";
-        a.innerHTML+="<img src='"+lienL+"' style='height: 1px!important'/>";
-        div.append(a);
-    }
-
+    });
 }
 
 const now = new Date();
-document.body.innerHTML=`<ul>
-<li><label>Cam</label> <select id='cam'>
-<option value="48043a4cf8b55d0019594df225f998b3">Tour</option>
-<option value="a2cc7835d2d58445e6615efebe5bbeaf">Stade</option>
-<option value="55a4eb940cb294f7cad1235a8e87b7bf">Sud</option>
-</select></li>
-<li><label>Date et Heure</label> <input type='datetime-local' id='datetime-local' value="${now.toISOString().slice(0,16)}" step="300"></li>
-<li><button onclick='go()'>Go</button></li>
-</ul>
-<div id="imgContainer"></div>
-`
+document.body.innerHTML = `
+    <ul>
+        <li><label>Date et Heure</label> <input type='datetime-local' id='datetime-local' value="${now.toISOString().slice(0,16)}" step="300"></li>
+        <li><button onclick='go()'>Go</button></li>
+    </ul>
+    <div class="flex">
+        <div class="imgContainer" id="48043a4cf8b55d0019594df225f998b3"></div>
+        <div class="imgContainer" id="a2cc7835d2d58445e6615efebe5bbeaf"></div>
+        <div class="imgContainer" id="55a4eb940cb294f7cad1235a8e87b7bf"></div>
+    </div>
+`;
 
-function zoom(e){
-    var zoomer = e.currentTarget;
-    e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX
-    e.offsetY ? offsetY = e.offsetY : offsetX = e.touches[0].pageX
-    x = offsetX/zoomer.offsetWidth*100
-    y = offsetY/zoomer.offsetHeight*100
-    zoomer.style.backgroundPosition = x + '% ' + y + '%';
+const zoom = (e) => {
+    const zoomer = e.currentTarget;
+    const offsetX = e.offsetX ? e.offsetX : e.touches[0].pageX;
+    const offsetY = e.offsetY ? e.offsetY : e.touches[0].pageX;
+    const x = (offsetX / zoomer.offsetWidth) * 100;
+    const y = (offsetY / zoomer.offsetHeight) * 100;
+    zoomer.style.backgroundPosition = `${x}% ${y}%`;
 }
 
 go();
