ed7a4c6bc491773ae0c33bb4e9f075efa6bf11ae
Maitre_Guigz <guillaume.hirschler@gmail.com>
2025-05-20 08:40:24 +0200

    Update main.js

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ const go = () => {
         console.log(id);
         cam = id;
         div = document.createElement("div");
-        for (let i = 0; i < 12; i++) {
+        for (let i = 0; i < 30; i++) {
             const a = document.createElement("a");
             const suffix = i < 10 ? `0${i}` : i;
             const lienH = `${url}${cam}/HIGH/${laDate}${suffix}.jpg`;
